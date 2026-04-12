a9c81036c988091b6db9492e0bc1300a1d193897
Luna Borella <ulfrthewolf@gmail.com>
2026-04-13 00:20:22 +1000

    Added tiles, shuffling the deck and dealing hands

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,35 @@
 module Main where
 
+import Control.Monad
+import Control.Monad.State
+import Data.Array
+import Tiles
+import Utils
+
+-- The state of the game at any given point in time
+data GameState = GS
+  { _deck :: Deck,
+    _hands :: Array Int Hand
+  }
+  deriving (Show)
+
+-- Draws a hand from the deck. State transformation where the deck is the state
+takeHand :: State Deck Hand
+takeHand = do
+  deck <- get
+  let hand = take 13 deck
+  put $ drop 13 deck
+  return $ listArray (0, 12) hand
+
+-- Creates a new game state (i.e., shuffles the deck, deals hands to players etc)
+newGameState :: IO GameState
+newGameState = do
+  shuffledDeck <- shuffle baseDeck
+  let (hands, deck) = runState (listArray (0, 3) <$> replicateM 4 takeHand) shuffledDeck
+  return GS {_deck = deck, _hands = hands}
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Welcome to MAHJONG"
+  state <- newGameState
+  print state
---
+++ app/Tiles.hs
@@ -0,0 +1,58 @@
+module Tiles where
+
+import Data.Array
+
+data Suit = Man | Pin | Sou deriving (Ord, Eq)
+
+data Colour = Red | Green | White deriving (Ord, Eq)
+
+data Direction = North | South | East | West deriving (Ord, Eq)
+
+type Rank = Int
+
+-- TODO red 5?
+data Tile = Number Suit Rank | Dragon Colour | Wind Direction deriving (Ord, Eq)
+
+type Hand = Array Int Tile
+
+type Deck = [Tile]
+
+-- A standard mahjong deck. Not in random order.
+baseDeck :: Deck
+baseDeck = concat $ replicate 4 quarterDeck
+
+-- One of each type of mahjong tile; i.e., a quarter of a standard deck.
+quarterDeck :: Deck
+quarterDeck =
+  [Number s r | s <- [Man, Pin, Sou], r <- [1 .. 9]]
+    ++ (Dragon <$> [Red, Green, White])
+    ++ (Wind <$> [East, North, West, South])
+
+showHand :: Hand -> String
+showHand = concatMap show
+
+showWind :: Direction -> String
+showWind dir = case dir of
+  East -> "🀀"
+  South -> "🀁"
+  West -> "🀂"
+  North -> "🀃"
+
+showDragon :: Colour -> String
+showDragon colour = case colour of
+  Red -> "🀄"
+  Green -> "🀅"
+  White -> "🀆"
+
+numberTileChars :: Suit -> [String]
+numberTileChars Man = ["🀇", "🀈", "🀉", "🀊", "🀋", "🀌", "🀍", "🀎", "🀏"]
+numberTileChars Sou = ["🀐", "🀑", "🀒", "🀓", "🀔", "🀕", "🀖", "🀗", "🀘"]
+numberTileChars Pin = ["🀙", "🀚", "🀛", "🀜", "🀝", "🀞", "🀟", "🀠", "🀡"]
+
+showNumberTile :: Suit -> Int -> String
+showNumberTile suit rank = numberTileChars suit !! (rank - 1)
+
+instance Show Tile where
+  show (Wind dir) = showWind dir
+  show (Dragon colour) = showDragon colour
+  show (Number suit rank) = showNumberTile suit rank
